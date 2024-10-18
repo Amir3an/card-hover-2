025deffba1ae72d9037f2ff35c61249b0375b1a1
Amir3an <amiralisayad0019@gmail.com>
2024-10-18 14:08:40 +0330

    finish

--- script.js
+++ script.js
@@ -5,50 +5,50 @@ const img1 = document.querySelector(".img1");
 const img2 = document.querySelector(".img2");
 const img3 = document.querySelector(".img3");
 
-img1.addEventListener("mouseover" , () => {
-    show1.style.transform = "translateY(-165px)";
-    setTimeout(() => {
-        show1.style.transform = "translateY(-110px)";
-        img1.style.overflow = "initial";
-    }, 500)
+img1.addEventListener("mouseover", () => {
+  show1.style.transform = "translateY(-165px)";
+  setTimeout(() => {
+    show1.style.transform = "translateY(-110px)";
+    img1.style.overflow = "initial";
+  }, 500);
 });
 
-img1.addEventListener("mouseout" , () => {
-    show1.style.transform = "translateY(-165px)";
-    setTimeout(() => {
-        show1.style.transform = "translateY(0px)";
-        img1.style.overflow = "hidden";
-    }, 500)
+img1.addEventListener("mouseout", () => {
+  show1.style.transform = "translateY(-165px)";
+  setTimeout(() => {
+    show1.style.transform = "translateY(0px)";
+    img1.style.overflow = "hidden";
+  }, 500);
 });
 
-img2.addEventListener("mouseover" , () => {
-    show2.style.transform = "translateY(-165px)";
-    setTimeout(() => {
-        show2.style.transform = "translateY(-110px)";
-        img2.style.overflow = "initial";
-    }, 500)
+img2.addEventListener("mouseover", () => {
+  show2.style.transform = "translateY(-165px)";
+  setTimeout(() => {
+    show2.style.transform = "translateY(-110px)";
+    img2.style.overflow = "initial";
+  }, 500);
 });
 
-img2.addEventListener("mouseout" , () => {
-    show2.style.transform = "translateY(-165px)";
-    setTimeout(() => {
-        show2.style.transform = "translateY(0px)";
-        img2.style.overflow = "hidden";
-    }, 500)
+img2.addEventListener("mouseout", () => {
+  show2.style.transform = "translateY(-165px)";
+  setTimeout(() => {
+    show2.style.transform = "translateY(0px)";
+    img2.style.overflow = "hidden";
+  }, 500);
 });
 
-img3.addEventListener("mouseover" , () => {
-    show3.style.transform = "translateY(-165px)";
-    setTimeout(() => {
-        show3.style.transform = "translateY(-110px)";
-        img3.style.overflow = "initial";
-    }, 500)
+img3.addEventListener("mouseover", () => {
+  show3.style.transform = "translateY(-165px)";
+  setTimeout(() => {
+    show3.style.transform = "translateY(-110px)";
+    img3.style.overflow = "initial";
+  }, 500);
 });
 
-img3.addEventListener("mouseout" , () => {
-    show3.style.transform = "translateY(-165px)";
-    setTimeout(() => {
-        show3.style.transform = "translateY(0px)";
-        img3.style.overflow = "hidden";
-    }, 500)
+img3.addEventListener("mouseout", () => {
+  show3.style.transform = "translateY(-165px)";
+  setTimeout(() => {
+    show3.style.transform = "translateY(0px)";
+    img3.style.overflow = "hidden";
+  }, 500);
 });
